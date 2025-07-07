8327457b9907f1a54db0ba975f8b06538c2fccdf
adammroczkowski <mroczkowskiadammm@gmail.com>
2025-07-07 18:47:53 +0200

    feat: add unit tests for empty input and single-number input

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,4 +1,11 @@
 package org.example;
 
 class StringCalculator {
+
+    public int add(String input) {
+        if(input.isEmpty()){
+            return 0;
+        }
+        return Integer.parseInt(input.trim());
+    }
 }
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -1,5 +1,38 @@
 package org.example;
 
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 class StringCalculatorTest {
+
+    @Test
+    void add_shouldReturnZero_whenInputIsEmpty() {
+        //give
+        StringCalculator calculator = new StringCalculator();
+
+        //when
+        var result = calculator.add(StringUtils.EMPTY);
+
+        //then
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void add_shouldReturnSameNumber_whenInputIsSingleNumber(){
+        //give
+        String value = "34";
+        StringCalculator calculator = new StringCalculator();
+
+        //when
+        var result = calculator.add(value);
+
+        //then
+        assertThat(result).isEqualTo(Integer.parseInt(value));
+
+    }
 
 }
