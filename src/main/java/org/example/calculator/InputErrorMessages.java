afb21f638bfe8c90b39f7bcd713d84e99f6bd0b7
adammroczkowski <mroczkowskiadammm@gmail.com>
2025-07-09 08:22:33 +0200

    refactor: reformat code and remove unused code

--- src/main/java/org/example/calculator/InputErrorMessages.java
+++ src/main/java/org/example/calculator/InputErrorMessages.java
@@ -1,7 +1,6 @@
 package org.example.calculator;
 
 final class InputErrorMessages {
-    private InputErrorMessages() {}
     static final String TRAILING_SEPARATOR      = "Separator at end not allowed";
     static final String NEGATIVE_NUMBERS_PREFIX = "Negative number(s) not allowed: ";
 }
